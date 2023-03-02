93c934d8760d1d86b6c6ee65549849c763ecfbb7
Maria89698 <maria9parhomova@gmail.com>
2023-03-02 19:17:15 +0100

    робить 7 дз

---
+++ index.js
@@ -0,0 +1,33 @@
+// #1
+const user {
+    userName = "Maria",
+    age = 14,
+    userName.showUserName();
+    userName.updateAge(1)
+}
+
+// #2
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+const sum = sum(salaries.values);
+
+// #3, 4
+const calculator = {
+    read(a, b),
+    sum(),
+    mult()
+}
+console.log(Object.entries(calculator));
+
+
+const cafe = {
+    name: "Republic",
+    width:200,
+    height: 300,
+    personal:5,
+    hr: true
+}
+console.log(mult(cafe.width, cafe.height));
